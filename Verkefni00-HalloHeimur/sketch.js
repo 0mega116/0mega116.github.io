e2809d94e9e00b329f381d13e7f57235f99a93dd
0mega116 <80836568+0mega116@users.noreply.github.com>
2021-03-25 10:34:51 +0000

    Verkefni00-HalloHeimur

--- Verkefni00-HalloHeimur/sketch.js
+++ Verkefni00-HalloHeimur/sketch.js
@@ -1,12 +1,17 @@
 // Hér kemur kóðinn þinn:
 
-function setup() {
-  createCanvas(400, 400);
-  background(255,200,0);
-  frameRate(4);
-}
-
-function draw() {
-  fill(random(0,255));
-  ellipse(200,200,50,50);
+function setup(){
+    createCanvas(400,300);
+    background(255,0,0);
+    fill(150);
+    ellipse(100,100,60,75);
+    fill(0);
+    textSize(20);
+    text("Halló heimur",200,40);
+    rect(200,200,50,50,20,0,20,0);
+    fill(0,0,255,);
+    ellipse(280,170,50,50);
+    fill(255);
+    text("Halló forrit!",210,100);
+    rect(80,180,70,30);
 }
