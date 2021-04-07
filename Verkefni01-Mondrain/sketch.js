ed41555332f23a0b7bba7c9a35b411be95635738
0mega116 <80836568+0mega116@users.noreply.github.com>
2021-04-07 09:41:20 +0000

    Verkefni01-Mondrain

---
+++ Verkefni01-Mondrain/sketch.js
@@ -0,0 +1,13 @@
+// Hér kemur kóðinn þinn:
+
+function setup(){
+    createCanvas(595,595);
+    background(14,14,16)
+    fill(195,45,35);
+    rect(0,0,274,343);
+    fill(237,237,237);
+    rect(286,0,309,343);
+    rect(0,356,275,239);
+    rect(265,333,330,262);
+    rect(558,450,37,145);
+}
