1d9099a059c788a1647b6082a537eff992c2065c
0mega116 <80836568+0mega116@users.noreply.github.com>
2021-04-19 11:34:47 +0000

    Verkefni02-Lifandi mynd klárað

--- Verkefni02-Lifandi mynd/sketch.js
+++ Verkefni02-Lifandi mynd/sketch.js
@@ -1,19 +1,21 @@
+var form;
+
 function setup() {
       createCanvas(400, 400);
       noStroke();
       colorMode(HSB);
-      frameRate(1);
+      frameRate(2);
+      rectMode(CENTER);
     }
 
     function draw() {
       background(0,0,100);
-
-      //teiknaStjörnu
-      beginShape();
       fill(random(0,359),100,50);
-      ellipse(0,0,400,400);
-      ellipse(400,0,400,400);
-      ellipse(0,400,400,400);
-      ellipse(400,400,400,400);
-      endShape();
+      form = int(random(0,2));
+        if(form == 0){
+          rect(random(0,400),random(0,400),random(40,80),random(40,80));
+        }
+        else {
+          ellipse(random(0,400),random(0,400),random(30,100));
+        }
     }
