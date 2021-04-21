0ec1a480b06a16adb580aeeef3b20a3a2c9722f3
0mega116 <80836568+0mega116@users.noreply.github.com>
2021-04-21 14:59:01 +0000

    Verkefni06-Skjáskipting næstum klárað

--- Verkefni06-Skjáskipting/sketch.js
+++ Verkefni06-Skjáskipting/sketch.js
@@ -1,11 +1,57 @@
 function setup(){
       createCanvas(700,520);
-      background(51);
       noStroke();
     }
 
 function draw(){
+      background(51);
       if(mouseX < width/2 && mouseY < height/2){
+        fill(255,200,0);
         rect(0,0,width/2,height/2);
+
+        if(mouseIsPressed) {
+          fill(255,200,0);
+          rect(0,0,width,height);
+        }
+      }
+      else if (mouseX > width/2 && mouseY < height/2) {
+        fill(255,0,40);
+        rect(width/2,0,width/2,height/2);
+
+        if(mouseIsPressed) {
+          fill(255,0,40);
+          rect(0,0,width,height);
+        }
+      }
+      else if (mouseX < width/2 && mouseY > height/2) {
+        fill(0,255,0);
+        rect(0,height/2,width/2,height/2);
+
+        if(mouseIsPressed) {
+          fill(0,255,0);
+          rect(0,0,width,height);
+        }
+      }
+      else if(mouseX >width/2 && mouseY >height/2) {
+        fill(0,0,255);
+        rect(width/2,height/2,width/2,height/2);
+
+        if(mouseIsPressed) {
+          fill(0,0,255);
+          rect(0,0,width,height);
+        }
       }
     }
+
+function keyPressed() {
+  if(keyCode == 32) {
+    fill(255,200,0);
+    rect(0,0,width/2,height/2);
+    fill(255,0,40);
+    rect(width/2,0,width/2,height/2);
+    fill(0,255,0);
+    rect(0,height/2,width/2,height/2);
+    fill(0,0,255);
+    rect(width/2,height/2,width/2,height/2);
+  }
+}
