1230af1fa389808c8eff2b831dfe27889905e78e
0mega116 <80836568+0mega116@users.noreply.github.com>
2021-04-26 16:02:49 +0000

    Verkefni04 og Verkefni06

--- Verkefni04-Lykkjuáskorun/sketch.js
+++ Verkefni04-Lykkjuáskorun/sketch.js
@@ -1,1 +1,17 @@
-// Hér kemur kóðinn þinn:
+
+function setup(){
+      createCanvas(400,400);
+      background(255,215,0);
+      strokeWeight(2);
+      colorMode(HSB);
+      frameRate(2);
+    }
+
+function draw(){
+      for(var x = 0; x < width; x = x+20){
+        fill(random(0,360),75,100);
+        for(var y = 0; y < height; y = y+20){
+          ellipse(10 + x, 10 + y, 15, 15);
+        }
+      }
+    }
--- Verkefni06-Skjáskipting/sketch.js
+++ Verkefni06-Skjáskipting/sketch.js
@@ -1,3 +1,5 @@
+var takki = false;
+
 function setup(){
       createCanvas(700,520);
       noStroke();
@@ -6,11 +8,11 @@ function setup(){
 function draw(){
       background(51);
       if(mouseX < width/2 && mouseY < height/2){
-        fill(255,200,0);
+        fill(255,215,0);
         rect(0,0,width/2,height/2);
 
         if(mouseIsPressed) {
-          fill(255,200,0);
+          fill(255,215,0);
           rect(0,0,width,height);
         }
       }
@@ -41,17 +43,20 @@ function draw(){
           rect(0,0,width,height);
         }
       }
+      if(takki) {
+          fill(255,215,0);
+          rect(0,0,width/2,height/2);
+          fill(255,0,40);
+          rect(width/2,0,width/2,height/2);
+          fill(0,255,0);
+          rect(0,height/2,width/2,height/2);
+          fill(0,0,255);
+          rect(width/2,height/2,width/2,height/2);
+      }
     }
 
 function keyPressed() {
   if(keyCode == 32) {
-    fill(255,200,0);
-    rect(0,0,width/2,height/2);
-    fill(255,0,40);
-    rect(width/2,0,width/2,height/2);
-    fill(0,255,0);
-    rect(0,height/2,width/2,height/2);
-    fill(0,0,255);
-    rect(width/2,height/2,width/2,height/2);
+    takki = !takki;
   }
 }
