895e159a6b36ee145055a00342ad8c4001ae41a0
0mega116 <80836568+0mega116@users.noreply.github.com>
2021-04-28 14:51:28 +0000

    Verkefni05-Lykkjulist upp

--- Verkefni05-Lykkjulist/sketch.js
+++ Verkefni05-Lykkjulist/sketch.js
@@ -1,1 +1,15 @@
-// Hér kemur kóðinn þinn:
+
+function setup() {
+  createCanvas(400,400);
+  background(255,215,0);
+  strokeWeight(2);
+  colorMode(HSB);
+  frameRate(2);
+}
+
+function draw() {
+  for(var i = 0; i < 13; i = i+1){
+    fill(random(0,360),75,100);
+    ellipse(70 + i*20, 180, 15, 15);
+  }
+}
