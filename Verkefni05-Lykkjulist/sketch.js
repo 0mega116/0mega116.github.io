9b490b14dbdeba3eaa9c0fd50798b1809545d35f
0mega116 <80836568+0mega116@users.noreply.github.com>
2021-04-29 11:59:03 +0000

    Verkefni07-Skopp upp

--- Verkefni05-Lykkjulist/sketch.js
+++ Verkefni05-Lykkjulist/sketch.js
@@ -5,11 +5,23 @@ function setup() {
   strokeWeight(2);
   colorMode(HSB);
   frameRate(2);
+  rectMode(CENTER);
 }
 
 function draw() {
+  //Teiknar efri línu af hringjum
   for(var i = 0; i < 13; i = i+1){
     fill(random(0,360),75,100);
-    ellipse(70 + i*20, 180, 15, 15);
+    ellipse(50 + i*25, 150, 20, 20);
+  }
+  //Teiknar línu af ferningum
+  fill(random(0,360),75,100);
+  for(var r = 0; r < 9; r = r+1){
+    rect(100 + r*25, 200, 20, 20);
+  }
+  //Teiknar neðri línu af hringjum
+  for(var e = 0; e < 11; e = e+1){
+    fill(random(0,360),75,100);
+    ellipse(75 + e*25, 250, 20, 20);
   }
 }
--- Verkefni07-Skopp/sketch.js
+++ Verkefni07-Skopp/sketch.js
@@ -1,1 +1,66 @@
 // Hér kemur kóðinn þinn:
+var boltiX = 100;
+var boltiY = 100;
+var hradiX = 4;
+var hradiY = -5;
+var boltiStaerd = 20;
+var stig = 0;
+var spadiBreidd = 150;
+var spadiThykkt = 25;
+var spadiX;
+var spadiY;
+
+function setup() {
+	createCanvas(600,500);
+	spadiY = height - 45;
+	rectMode(CENTER);
+	textFont("Courier New",16);
+	textStyle(BOLD);
+}
+
+function draw() {
+  spadiX = mouseX;
+  background(255,200,0);
+	// Reikna ný hnit út frá hraða boltans:
+  boltiX = boltiX + hradiX;
+  boltiY = boltiY + hradiY;
+	// Athuga hvort boltinn snertir veggina:
+  if ((boltiX > width-boltiStaerd/2) || boltiX < boltiStaerd/2)  {
+    hradiX = hradiX * -1;
+  }
+  	// Athuga hvort boltinn snertir þakið:
+  if ((boltiY < boltiStaerd)) {
+    hradiY = hradiY * -1;
+  }
+  // Athugar hvort boltinn snertir gólfið:
+  if(boltiY > height-boltiStaerd/2) {
+    hradiY = hradiY *-1;
+    if(stig > 0) {
+      stig = stig - 1;
+    }
+  }
+  // Athuga hvort bolti snertir spaða:
+  if(abs(spadiY - boltiY) < boltiStaerd/2 + spadiThykkt/2 && abs(spadiX - boltiX) < boltiStaerd/2 + spadiBreidd/2){
+    hradiY = -abs(hradiY * -1);
+    stig = stig + 1;
+  }
+  // Athuga hvort spaði snertir vegginn vinstra megin:
+  if(mouseX - spadiBreidd/2 < 0) {
+    spadiX = spadiBreidd/2
+  }
+  // Athuga hvort spaði snertir vegginn hægra megin:
+  if(mouseX + spadiBreidd/2 >= width) {
+    spadiX = width-spadiBreidd/2
+  }
+	// Teikna boltann
+  fill(236,36,94);
+  ellipse(boltiX, boltiY, boltiStaerd, boltiStaerd);
+	// Teikna spaðann
+  fill(3,123,227);
+  rect(spadiX,spadiY ,spadiBreidd,spadiThykkt);
+  fill (0);
+  ellipse(spadiX, spadiY, 10,10);
+	// Teikna stigin
+  fill(0);
+  text("Stig: " + stig,10,20);
+}
